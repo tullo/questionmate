7cc938f70219aec4b3d3a2b67cf6265e54ac5e7a
Ralf Wirdemann <ralf.wirdemann@codekeepers.de>
2020-07-14 17:17:35 +0200

    Deletes Questionaire type

--- domain/types.go
+++ domain/types.go
@@ -1,11 +1,10 @@
 package domain
 
 import (
-	"fmt"
-	"sort"
 	"strconv"
 )
 
+// todo add number remaining questions
 type Question struct {
 	ID           int       `json:"id"`
 	Text         string    `json:"text"`
@@ -53,50 +52,6 @@ func NewOption(value int, text string) *Option {
 
 type Score struct {
 	Value int
-}
-
-// todo delete type Questionaire
-type Questionaire struct {
-	Questions map[int]*Question // Questions maps Questions by their IDs
-}
-
-func NewQuestionnaire() Questionaire {
-	q := Questionaire{}
-	q.Questions = make(map[int]*Question)
-	return q
-}
-
-// Unanswered returns an sorted array of unanswered question ids according to
-// the given answers.
-func (q Questionaire) Unanswered(answers []Answer) []int {
-	var unanswered []int
-	for id := range q.Questions {
-		if !contains(id, answers) {
-			unanswered = append(unanswered, id)
-		}
-	}
-	sort.Ints(unanswered)
-	return unanswered
-}
-
-func contains(id int, answers []Answer) bool {
-	for _, a := range answers {
-		if a.QuestionID == id {
-			return true
-		}
-	}
-	return false
-}
-
-func (q Questionaire) String() string {
-	var s string
-	for _, question := range q.Questions {
-		s = fmt.Sprintf("%sQuestion: %s\n", s, question.Text)
-		for _, option := range question.Options {
-			s = fmt.Sprintf("%s- %s\n", s, option.Text)
-		}
-	}
-	return s
 }
 
 type Answer struct {
